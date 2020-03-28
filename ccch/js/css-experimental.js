c17c5cdef841040ad44fe0baff6f2cd3f6a50815
Michael Glünz <michael.gluenz@gmail.com>
2020-03-28 09:14:16 +0000

    Zugefügt: js/css-experimental.js

---
+++ ccch/js/css-experimental.js
@@ -0,0 +1,15 @@
+$("main ul").removeClass("default");
+$("main ul").addClass("list-group");
+$("main li").removeClass("default");
+$("main li").addClass("list-group-item");
+$("main li").addClass("b1");
+$("h1").addClass("p0b");
+$("h2").addClass("c03");
+$("h3").addClass("c04");
+$("p").addClass("p01");
+$("li").addClass("c01");
+$(".cta").addClass("b5")
+$(".cta").addClass("c12")
+$(".cta > span:nth-child(1)").addClass("c12")
+$(".cta > span:nth-child(3)").addClass("cw")
+$(".cta > span:nth-child(5)").addClass("cw")
